018ef91facb7c9e3080cb5d2e1849b92a901aae3
shinya-git-jp <ritsuki-shinya@kccs.co.jp>
2025-10-21 12:04:53 +0900

    test

--- view/src/message/MessageResourceConvert.tsx
+++ view/src/message/MessageResourceConvert.tsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { useState, useRef } from "react";
 import "../App.css";
 
+
 interface EditableLabel {
   objectID: string;
   categoryName: string;
